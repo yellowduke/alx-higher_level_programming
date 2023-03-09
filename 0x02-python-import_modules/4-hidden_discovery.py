9767e516bd88becebf473c5a6c441b3f839ca7a8
yellowduke <yelohduke@gmail.com>
2023-03-09 13:07:24 -0800

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
-f __name__ == "__main__":
+#!/usr/bin/python3
+if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{:s}".format(i))
