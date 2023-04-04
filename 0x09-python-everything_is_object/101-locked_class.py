7d4576ab43c1a9f4b3a3867d23783dda7d9c6637
yellowduke <yelohduke@gmail.com>
2023-04-04 13:40:51 -0700

    initial commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,7 +8,7 @@ class LockedClass:
     Prevents the user from dynamically creating new instance attributes except new intamce attribute is called first_name
     """
 
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
 
     def __init__(self, first_name):
-        self.first_name = first_name
+        pass
