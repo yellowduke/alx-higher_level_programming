7b7f8f2b60557b2ef232055670146fa16b70eadd
yellowduke <yelohduke@gmail.com>
2023-04-04 13:38:14 -0700

    initial commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -12,8 +12,3 @@ class LockedClass:
 
     def __init__(self, first_name):
         self.first_name = first_name
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError("Cannot add new attribute")
-        self.__dict__[name] = value
