e366946b11f116ece8eb3039599b03c131b2729d
yellowduke <yelohduke@gmail.com>
2023-04-04 13:10:40 -0700

    initial commit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n):
-    return ''.join(['BestSchool' + str(i) for i in range(1, n+1)])
+def magic_string(n=[]):
+    n += ["BestSchool"]
+    return ", ".join(n)
