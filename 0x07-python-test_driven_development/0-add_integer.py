97bcec02630d9a65e2a754d8efb5c683750f4ac5
yellowduke <yelohduke@gmail.com>
2023-04-04 17:46:05 -0700

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Defines an integer addition function.
+"""
+
+def add_integer(a, b=98):
+    """
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
