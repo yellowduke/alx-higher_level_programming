a6d004fc0071311ce410500d411a0afd969b915b
yellowduke <yelohduke@gmail.com>
2023-04-12 14:30:41 -0700

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that contains a function that reads a text file. """
 
+
 def read_file(filename=""):
     """ Print the contents of a UTF8 text file to stdout.  """
 
