bbea6da7badfa0714bb07e3501dd6ed300449754
yellowduke <yelohduke@gmail.com>
2023-05-17 19:12:49 -0700

    Queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_s.title, tv_s_genres.genre_id
-  FROM `tv_shows` AS tv-s
-       LEFT JOIN tv_show_genres AS tv_s_genres
-       ON tv_s.id = tv_s_genres.show_id
- ORDER BY tv_s.title, tv_s_genres.genre_id;
+  FROM `tv_shows` AS tv_s
+LEFT JOIN tv_show_genres AS tv_s_genres
+ON tv_s.id = tv_s_genres.show_id
+ORDER BY tv_s.title, tv_s_genres.genre_id;
