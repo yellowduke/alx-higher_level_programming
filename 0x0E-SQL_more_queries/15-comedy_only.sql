a22f9828f5e7577de9706fbe27f6ef3369a09e5e
yellowduke <yelohduke@gmail.com>
2023-05-18 20:02:24 -0700

    Comedy only

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,10 @@
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_s.title
+FROM tv_shows AS tv_s
+INNER JOIN tv_show_genres AS tv_s_g
+ON tv_s.id = tv_s_g.show_id
 
+INNER JOIN tv_genres AS tv_g
+ON tv_g.id = tv_s_g.genre_id
+WHERE tv_g.name = "Comedy"
+ORDER BY tv_s.title;
