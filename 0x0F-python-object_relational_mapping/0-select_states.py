e6fcc861daa950a500d1b750f4860182ece9668f
yellowduke <yelohduke@gmail.com>
2023-06-17 03:05:55 -0700

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+	db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	csr = db.cursor()
+	csr.execute("SELECT * FROM states ORDER BY states.id ASC")
+	
+	for state in csr.fetchall():
+		print(state)
+	csr.close()
+	bd.close()
