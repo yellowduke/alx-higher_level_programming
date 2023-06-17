8ee0889be6749a19955bb60dfaba3ecac8185729
yellowduke <yelohduke@gmail.com>
2023-06-17 03:25:08 -0700

    initial commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+""" Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
 
 import MySQLdb
 from sys import argv
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3])
     csr = db.cursor()
-    csr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    csr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for state in csr.fetchall():
         print(state)
         csr.close()
